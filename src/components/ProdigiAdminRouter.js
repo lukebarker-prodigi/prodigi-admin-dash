7c161b51b72fc57272cc9b98a0b13b18c2333a50
Luke Barker <luke.barker@prodigi.com>
2020-10-04 22:26:00 +0100

    Add links to pbas and report tools

--- src/components/ProdigiAdminRouter.js
+++ src/components/ProdigiAdminRouter.js
@@ -23,21 +23,16 @@ export default class ProdigiAdminRouter extends React.Component {
           <Nav className="mr-auto">
             <Nav.Link href="/home">Home</Nav.Link>
             <Nav.Link href="/merchant">Merchants</Nav.Link>
-            <Nav.Link href="/order">Order</Nav.Link>
+            <Nav.Link href="/order">Orders</Nav.Link>
             <Nav.Link href="/invoice">Invoices</Nav.Link>
             <Nav.Link href="/issue">Issues</Nav.Link>
-            <Nav.Link href="/sku">SKUs</Nav.Link>
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown> */}
+            <Nav.Link href="/sku">Magic SKU Box</Nav.Link>
+            <Nav.Link href="/allocation">PBAS</Nav.Link>
+            <Nav.Link href="/report">CS Reports</Nav.Link>
           </Nav>
           <Form inline>
             <FormControl type="text" placeholder="Order ID/etc." className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
+            <Button variant="outline-info">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Navbar>
