fb3c5619d1184d1e0c71024bcdd6c6aaebd4e6ff
Luke Barker <luke.barker@prodigi.com>
2020-10-07 20:12:17 +0100

    Add spans to h4 tags

--- src/components/Dashboard/ProductionOrders.js
+++ src/components/Dashboard/ProductionOrders.js
@@ -9,7 +9,9 @@ export default class ProductionOrders extends React.Component {
   render() {
     return (
       <div style={{margin: "20px"}}>
-        <h4 style={{marginBottom: "20px"}}>Production Orders</h4>
+        <h4 style={{marginBottom: "20px"}}>
+          <span>Production Orders</span>
+        </h4>
         <Table bordered hover>
           <tr class="table-primary">
             <td>Orders in Progress</td>
@@ -28,7 +30,9 @@ export default class ProductionOrders extends React.Component {
             <th>34</th>
           </tr>
         </Table>
-        <h4 style={{marginBottom: "20px"}}>Today's Top Stats</h4>
+        <h4 style={{marginBottom: "20px"}}>
+          <span>Today's Top Stats</span>
+        </h4>
         <Table bordered hover>
           <tr class="table-secondary">
             <td>Most Popular Lab</td>
